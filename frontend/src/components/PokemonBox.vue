63cd28b5a2b9a0e6afbc27503013bbca02b62fb5
Tân <baotan1909@gmail.com>
2025-07-30 15:57:13 +0700

    Add Team Storage

---
+++ frontend/src/components/PokemonBox.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-card class="pa-4 mb-4 rounded-xl" elevation="3">
+        <v-card-title class="text-h6 font-weight-bold">
+        {{ team.name }}
+        </v-card-title>
+
+        <v-divider class="my-2" />
+
+        <v-row justify="center" class="pokemon-list mb-2 flex-wrap" style="overflow-x: auto;">
+            <v-col
+            v-for="(p, i) in team.pokemons"
+            :key="i"
+            cols="auto"
+            class="d-flex justify-center"
+            >
+                <img
+                :src="`https://img.pokemondb.net/sprites/home/normal/${p?.species || '0'}.png`"
+                :alt="p?.species || 'Unknown'"
+                width="64"
+                height="64"
+                />
+            </v-col>
+        </v-row>
+
+        <v-divider class="my-2" />
+
+        <v-row align="center" class="mt-1">
+            <v-col cols="auto" class="d-flex align-center">
+                <v-icon icon="mdi-heart" color="red" start />
+                <span class="ml-1 font-weight-medium">{{ team.likes_count }} Likes</span>
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
+
+<script setup>
+    defineProps({
+        team: {
+            type: Object,
+            required: true,
+            validator: (t) =>
+            typeof t.id === 'number' &&
+            Array.isArray(t.pokemons) &&
+            typeof t.name === 'string'
+        }
+    })
+</script>
---
+++ frontend/src/components/TeamStorage.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <h1 class="mb-4">Your Stored Teams</h1>
+
+    <v-alert
+    v-if="Object.keys(teams).length === 0"
+    type="info"
+    variant="tonal"
+    class="mb-4"
+    >
+      You haven't created any teams yet.
+    </v-alert>
+
+    <v-row v-else>
+      <v-col
+      v-for="team in Object.values(teams)"
+      :key="team.id"
+      cols="12"
+      md="6"
+      >
+        <PokemonBox :team="team" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import PokemonBox from '../components/PokemonBox.vue'
+const teams = {}
+</script>
