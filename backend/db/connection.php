0e152cdf540297465507d4ff5798aa2fc30d7673
Tân <baotan1909@gmail.com>
2025-07-31 23:52:50 +0700

    Update db conn logic

--- backend/db/connection.php
+++ backend/db/connection.php
@@ -6,14 +6,30 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
+$dbPath = $_ENV['DB_PATH'];
+$initStmtPath = $_ENV['DB_INIT_PATH'];
+
 try {
-    $dbPath = $_ENV['DB_PATH'];
+    $isNewDb = !file_exists($dbPath);
+    $isIniStmtExist = file_exists($initStmtPath);
     $pdo = new PDO("sqlite:" . $dbPath);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->exec("PRAGMA foreign_keys = ON;");
-} catch (PDOException $e) {
+
+    if ($isNewDb && !$isIniStmtExist) {
+        throw new Exception("SQL schema file not found at: $initStmtPath");
+    }
+
+    if ($isNewDb) {
+        $schema = file_get_contents($initStmtPath);
+        $pdo->exec($schema);
+    }
+} catch (Exception $e) {
     http_response_code(500);
-    echo json_encode(['error' => 'Database connection failed.', 'details' => $e->getMessage()]);
+    echo json_encode([
+        'error' => 'Database initialization failed.',
+        'details' => $e->getMessage()
+    ]);
     exit;
 }
