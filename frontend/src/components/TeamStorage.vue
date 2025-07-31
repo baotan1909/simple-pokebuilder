6931b155dbc0a7629a2c643f3db0af2ff612fbd2
Tân <baotan1909@gmail.com>
2025-07-31 13:42:35 +0700

    Remove nested v-if + v-for in TeamStorage

--- frontend/src/components/TeamStorage.vue
+++ frontend/src/components/TeamStorage.vue
@@ -2,15 +2,15 @@
   <v-container>
     <h1 class="mb-4">Your Stored Teams</h1>
 
-  <v-alert v-if="!isAuthenticated || noTeams" type="info" variant="tonal" class="mb-4">
+  <v-alert v-if="!isAuthenticated || noTeam" type="info" variant="tonal" class="mb-4">
       {{ alertMessage }}
     </v-alert>
 
-    <v-row v-if="isAuthenticated && !noTeams">
-      <v-col v-for="team in teamList" :key="team.id" cols="12" md="6">
-        <PokemonBox :team="team" />
-      </v-col>
-    </v-row>
+    <v-row>
+  <v-col v-for="team in displayTeams" :key="team.id" cols="12" md="6">
+    <PokemonBox :team="team" />
+  </v-col>
+</v-row>
   </v-container>
 </template>
 
@@ -22,8 +22,9 @@ import PokemonBox from '../components/PokemonBox.vue'
 const { isAuthenticated } = useAuth()
 const teams = ref({})
 
-const noTeams = computed(() => Object.keys(teams.value).length === 0)
+const noTeam = computed(() => Object.keys(teams.value).length === 0)
 const teamList = computed(() => Object.values(teams.value))
+const displayTeams = computed(() => {return isAuthenticated.value && !noTeam.value ? teamList.value : []})
 const alertMessage = computed(() =>
   !isAuthenticated.value
     ? 'You need to log in to create and view your teams.'
